765f2428aa5292037537e18f8636984f35b599a6
Kelsie Murphy <kelsie@kelsiesmurphy.com>
2024-02-13 17:45:39 +0000

    Fixing API Key

--- src/lib/stripe/stripe.ts
+++ src/lib/stripe/stripe.ts
@@ -3,7 +3,7 @@ import { STRIPE_SECRET } from '$env/static/private';
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY_LIVE ?? STRIPE_SECRET ?? '', {
 	// https://github.com/stripe/stripe-node#configuration
-	apiVersion: '2022-11-15'
+	apiVersion: '2023-10-16'
 	// Register this as an official Stripe plugin.
 	// https://stripe.com/docs/building-plugins#setappinfo
 	// appInfo: {
