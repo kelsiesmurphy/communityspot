50ada1e774d0ef5ae2db4961e1783debd575cb5a
Kelsie Murphy <kelsie@kelsiesmurphy.com>
2024-02-14 09:51:20 +0000

    Removing Products

--- src/routes/products/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	import ProductCard from '$lib/components/ui/ProductCard.svelte';
-</script>
-
-<h1 class="h1 font-thin">Products Page</h1>
-
-<div class="grid grid-cols-3 gap-3">Work in progress</div>
