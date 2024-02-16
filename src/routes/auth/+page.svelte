aac4dd9a0bfce889c789822256a61be2f490d0dc
Kelsie Murphy <kelsie@kelsiesmurphy.com>
2024-02-16 10:06:22 +0000

    Fixes to signup

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -1,89 +1,79 @@
 <script lang="ts">
-	// import { goto } from '$app/navigation';
-	// import { Button } from '$lib/components/ui/button';
+	import { goto } from '$app/navigation';
+	import { Button } from '$lib/components/ui/button';
 
-	import * as Form from '$lib/components/ui/form';
-	import type { PageData } from './$types';
-	import { formSchema, type FormSchema } from '../../routes/auth/schema';
-	export let data: PageData;
+	export let data;
+	let { supabase, session } = data;
+	$: ({ supabase, session } = data);
 
-	// export let form: SuperValidated<FormSchema>;
-	import type { SuperValidated } from 'sveltekit-superforms';
+	if (session) {
+		goto('/profile');
+	}
 
-	// export let data;
-	// let { supabase, session } = data;
-	// $: ({ supabase, session } = data);
+	let email: string;
+	let password: string;
+	let loading = false;
 
-	// if (session) {
-	// 	goto('/profile');
-	// }
-	// /**
-	//  * @type {string}
-	//  */
-	// let email;
+	const handleSignIn = async () => {
+		try {
+			loading = true;
+			const { error } = await supabase.auth.signInWithPassword({
+				email,
+				password
+			});
+			if (error) throw error;
+			alert('Login successfully, visit your home page!');
+			goto('/profile');
+		} catch (error) {
+			if (error instanceof Error) {
+				alert(error.message);
+			}
+		} finally {
+			loading = false;
+		}
+	};
 
-	// /**
-	//  * @type {string}
-	//  */
-	// let password;
-
-	// let loading = false;
-
-	// const handleSignIn = async () => {
-	// 	try {
-	// 		loading = true;
-	// 		const { error } = await supabase.auth.signInWithPassword({
-	// 			email,
-	// 			password
-	// 		});
-	// 		if (error) throw error;
-	// 		alert('Login successfully, visit your home page!');
-	// 		goto('/profile');
-	// 	} catch (error) {
-	// 		if (error instanceof Error) {
-	// 			alert(error.message);
-	// 		}
-	// 	} finally {
-	// 		loading = false;
-	// 	}
-	// };
-
-	// const handleSignUp = async () => {
-	// 	try {
-	// 		loading = true;
-	// 		const { error } = await supabase.auth.signUp({
-	// 			email,
-	// 			password,
-	// 			options: {
-	// 				emailRedirectTo: `${location.origin}/auth/callback`
-	// 			}
-	// 		});
-	// 		if (error) throw error;
-	// 		alert('Your email must be verified, go check your email!');
-	// 	} catch (error) {
-	// 		if (error instanceof Error) {
-	// 			alert(error.message);
-	// 		}
-	// 	} finally {
-	// 		loading = false;
-	// 	}
-	// };
+	const handleSignUp = async () => {
+		try {
+			loading = true;
+			const { error } = await supabase.auth.signUp({
+				email,
+				password,
+				options: {
+					emailRedirectTo: `${location.origin}/auth/callback`
+				}
+			});
+			if (error) throw error;
+			alert('Your email must be verified, go check your email!');
+		} catch (error) {
+			if (error instanceof Error) {
+				alert(error.message);
+			}
+		} finally {
+			loading = false;
+		}
+	};
 </script>
 
-<!-- 
 <form>
 	<label>
 		<span>Email</span>
-		<input class="input" name="email" bind:value={email} />
+		<input name="email" bind:value={email} />
 	</label>
 	<label>
 		<span>Password</span>
-		<input class="input" type="password" name="password" bind:value={password} />
+		<input type="password" name="password" bind:value={password} />
 	</label>
 </form>
 
-<div class="btn-group variant-filled">
-	<button on:click|preventDefault={handleSignIn} disabled={loading}>Sign in</button>
+<div>
+	<Button
+		on:click={(event) => {
+			event.preventDefault();
+			handleSignIn();
+		}}
+		disabled={loading}>Sign in</Button
+	>
 	<Button
 		on:click={(event) => {
 			event.preventDefault();
@@ -91,16 +81,4 @@
 		}}
 		disabled={loading}>Sign Up</Button
 	>
-</div> -->
-
-<Form.Root method="POST" form={data.form} schema={formSchema} let:config debug={true}>
-	<Form.Field {config} name="username">
-		<Form.Item>
-			<Form.Label>Username</Form.Label>
-			<Form.Input />
-			<Form.Description>This is your public display name.</Form.Description>
-			<Form.Validation />
-		</Form.Item>
-	</Form.Field>
-	<Form.Button>Submit</Form.Button>
-</Form.Root>
+</div>
